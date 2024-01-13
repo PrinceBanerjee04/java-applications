16cf370a94461025d02cd13e33fcdf9d6be78767
PrinceBanerjee04 <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-01-13 11:55:46 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -37,3 +37,67 @@ public class TicTacToe extends JFrame implements ActionListener {
         }
         checkForWin();
     }
+private void checkForWin() {
+        
+        for (int i = 0; i < 3; i++) {
+            if (!buttons[i][0].getText().equals("") &&
+                buttons[i][0].getText().equals(buttons[i][1].getText()) &&
+                buttons[i][0].getText().equals(buttons[i][2].getText())) {
+                JOptionPane.showMessageDialog(this, buttons[i][0].getText() + " wins!");
+                resetGame();
+                return;
+            }
+            if (!buttons[0][i].getText().equals("") &&
+                buttons[0][i].getText().equals(buttons[1][i].getText()) &&
+                buttons[0][i].getText().equals(buttons[2][i].getText())) {
+                JOptionPane.showMessageDialog(this, buttons[0][i].getText() + " wins!");
+                resetGame();
+                return;
+            }
+        }
+        if (!buttons[0][0].getText().equals("") &&
+            buttons[0][0].getText().equals(buttons[1][1].getText()) &&
+            buttons[0][0].getText().equals(buttons[2][2].getText())) {
+            JOptionPane.showMessageDialog(this, buttons[0][0].getText() + " wins!");
+            resetGame();
+            return;
+        }
+        if (!buttons[0][2].getText().equals("") &&
+            buttons[0][2].getText().equals(buttons[1][1].getText()) &&
+            buttons[0][2].getText().equals(buttons[2][0].getText())) {
+            JOptionPane.showMessageDialog(this, buttons[0][2].getText() + " wins!");
+            resetGame();
+            return;
+        }
+        
+        boolean draw = true;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (buttons[i][j].getText().equals("")) {
+                    draw = false;
+                    break;
+                }
+            }
+            if (!draw) {
+                break;
+            }
+        }
+        if (draw) {
+            JOptionPane.showMessageDialog(this, "It's a draw!");
+            resetGame();
+        }
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        player1Turn = true;
+    }
+
+    public static void main(String[] args) {
+        new TicTacToe();
+    }
+}
